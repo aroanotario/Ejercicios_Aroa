913a48d3f05719f4e987c2ca4cac683cbd962a1f
aroan <aroan@192.168.1.137>
2016-11-10 20:35:05 +0100

    Creacion de la clase persona y coche

--- prueba1/src/prueba1/coche.java
+++ prueba1/src/prueba1/coche.java
@@ -1,19 +1,14 @@
-package prueba1;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package prueba1;
 
 /**
  *
  * @author aroan
  */
-public class persona {
-    
-    private String nombre;
-    private int edad;
-    
+public class coche {
     
 }
