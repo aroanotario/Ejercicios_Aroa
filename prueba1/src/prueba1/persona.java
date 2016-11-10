cc1e2f5faf9caa4433e6542f810aa03dec7d0421
aroan <aroan@192.168.1.137>
2016-11-10 20:33:21 +0100

    Creacion de la clase persona

---
+++ prueba1/src/prueba1/persona.java
@@ -0,0 +1,15 @@
+package prueba1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aroan
+ */
+public class persona {
+    
+}
